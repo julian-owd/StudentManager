9a564112acddd8280b1e8f047e8927a8c25c9f43
elckes1208 <elias-roth@gmx.net>
2022-05-08 22:00:40 +0200

    TestUI updated
    Methoden wurden getestet.
    getSubstitutionPla() lieferte eine leere ArrayList zurück - ist fehlerhaft

--- src/main/java/StudentManagerTestUI.java
+++ src/main/java/StudentManagerTestUI.java
@@ -1,7 +1,28 @@
+import manager.StudentManager;
+
 public class StudentManagerTestUI {
 
   public static void main(String[] args) {
-    System.out.println("Hallo.");
-    System.out.println("Hallo Kenny");
+    StudentManager sm = new StudentManager();
+
+//    System.out.println(sm.findCourse(1));
+//    System.out.println();
+//    System.out.println(sm.findCourse("12BGPI"));
+//    System.out.println();
+//    System.out.println(sm.findUser(4));
+//    System.out.println();
+//    System.out.println(sm.findUser("kai.tuschner@mail.com"));
+//    System.out.println();
+//    System.out.println(sm.findHomework(1));
+//    System.out.println();
+//    System.out.println(sm.logIn("elias.roth@gmx.net", "KatzeColaFlugzeugMathe"));
+//    System.out.println();
+//    sm.logOut();
+//    System.out.println();
+    System.out.println(sm.getSubstitutionPlan());
+//    System.out.println();
+//    sm.markHomeworkAsFinished(sm.findHomework(1));
+//    System.out.println();
+//    sm.deleteUser(sm.findUser(1));
   }
 }
