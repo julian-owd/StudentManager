6c9a438ecdde57d7b35caf0f4903d353f4af25df
Julian <50843622+julian-owd@users.noreply.github.com>
2022-05-16 11:13:55 +0200

    Added AccountManagement
    - Added MainMenu
    - Added AccountManagement
    - Added ChangeMail
    - Added methods in StudentManager to change the mail & password
    - Added password in User

--- src/main/java/gui/MainMenu.java
+++ src/main/java/gui/MainMenu.java
@@ -1,22 +1,63 @@
 package gui;
 
 import manager.StudentManager;
+import user.Teacher;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainMenu {
     private JPanel panel1;
+    private JButton meineKurseButton;
+    private JButton accountButton;
+    private JButton button1; // Vertretungsplan & Account hinfzufügen
+    private JButton button2; // Hausaufgaben & Account löschen
+    private JButton logoutButton;
+    private JLabel accountLabel;
+    private JButton hausaufgabeHinzufügenButton;
 
     public MainMenu(JFrame jFrame, StudentManager studentManager) {
         // configuring the jFrame
         jFrame.setTitle("Übersicht - Schulportal");
-        jFrame.setContentPane(panel1);
+        jFrame.setContentPane(this.panel1);
         jFrame.setPreferredSize(new Dimension(800, 500));
         jFrame.pack();
         jFrame.setLocationRelativeTo(null);
         jFrame.getRootPane().setDefaultButton(null);
 
-        panel1.setVisible(true);
+        this.accountLabel.setText(studentManager.getCurrentUser().getFirstName() + " " + studentManager.getCurrentUser().getLastName());
+        this.hausaufgabeHinzufügenButton.setVisible(false);
+
+        if (studentManager.getCurrentUser() instanceof Teacher t) {
+            this.hausaufgabeHinzufügenButton.setVisible(true);
+            if (t.isAdmin()) {
+                button1.setText("Account hinzufügen");
+                button2.setText("Account löschen");
+            } else {
+                button1.setVisible(false);
+                button2.setVisible(false);
+            }
+        }
+
+        this.panel1.setVisible(true);
+
+        this.logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                panel1.setVisible(false);
+                studentManager.logOut();
+                new Login(jFrame, studentManager);
+            }
+        });
+
+        this.accountButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                panel1.setVisible(false);
+                new AccountManagement(jFrame, studentManager);
+            }
+        });
     }
 }
